1fcd1ce8b7bd9f0ae46342a88c28710dd5c9f814
Gabriella Gracia <gabriellagraciamt@icloud.com>
2019-05-31 14:41:13 +0700

    bikin alert

--- LookForDifferences/ViewController.swift
+++ LookForDifferences/ViewController.swift
@@ -15,14 +15,48 @@ class ViewController: UIViewController {
     @IBOutlet weak var hardOutlet: UIButton!
     @IBOutlet weak var gifImage: UIImageView!
     
+    @IBAction func `do`(_ sender: Any)
+    {
+        let alert = UIAlertController(title: "Congratulation!!", message: "🥳", preferredStyle: .alert)
+        alert.addAction(.init(title: "Home", style: .default, handler:
+            {(UIAlertAction) in self.performSegue(withIdentifier: "keHome", sender: self)}))
+        alert.addAction(.init(title: "Cancel", style: .cancel, handler:
+            {(UIAlertAction) in print("cancel")}))
+        self.present(alert, animated: true)
+        {
+            
+        }
+    }
+    
+    
+    @IBAction func easyKeHome(_ sender: Any)
+    {
+        let alert = UIAlertController(title: "Congratulation", message: "🥳", preferredStyle: .alert)
+        alert.addAction(.init(title: "Home", style: .default, handler:
+            {(UIAlertAction) in self.performSegue(withIdentifier: "keHomeDariEasy", sender: self)}))
+        alert.addAction(.init(title: "Cancel", style: .cancel, handler:
+            {(UIAlertAction) in print("cancel")}))
+        self.present(alert, animated: true)
+        {
+            
+        }
+    }
+    
+    
+    
+    @IBAction func alertField(_ sender: UITextField)
+    {
+        
+    }
     
     override func viewDidLoad()
     {
         super.viewDidLoad()
 //        judulLabel.layer.borderColor = #colorLiteral(red: 0.8360164762, green: 0.06704514474, blue: 0.3524019718, alpha: 1)
 //        judulLabel.layer.borderWidth = 5
-        judulLabel.layer.cornerRadius = 20
+//        judulLabel.layer.cornerRadius = 20
         // Do any additional setup after loading the view.
+        
     }
     
     
